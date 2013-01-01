ef91bfeadf2dc4ce5996779b60e7b87ae7d4a5bf
Jay <jay@Kenneths-MacBook.local>
2013-01-01 14:52:19 -0500

    Initial Commit

---
+++ JATickerView/JATickerView.h
@@ -0,0 +1,13 @@
+//
+//  JATickerView.h
+//  JATickerView
+//
+//  Created by Jay on 1/1/13.
+//  Copyright (c) 2013 jaysapps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JATickerView : NSObject
+
+@end
---
+++ JATickerView/JATickerView.m
@@ -0,0 +1,13 @@
+//
+//  JATickerView.m
+//  JATickerView
+//
+//  Created by Jay on 1/1/13.
+//  Copyright (c) 2013 jaysapps. All rights reserved.
+//
+
+#import "JATickerView.h"
+
+@implementation JATickerView
+
+@end
